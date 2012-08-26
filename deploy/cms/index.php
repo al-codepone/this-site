cb512ab416f3b762a67df0e4f60b30b9f527a61a
Ry Ferguson <rytf12@gmail.com>
2012-08-26 00:52:41 -0700

    replace functions include with getRoute

--- deploy/cms/index.php
+++ deploy/cms/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('../constants.php');
-require_once(CITY_PHP . 'functions.php');
+require_once(CITY_PHP . 'getRoute.php');
 require_once(THIS_SITE_PHP . 'database/MyModelFactory.php');
 require_once(THIS_SITE_PHP . 'html/cmsNavItems.php');
 require_once(THIS_SITE_PHP . 'html/urlDupError.php');
