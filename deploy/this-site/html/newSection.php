95dfb4e85b8860b82909f8322a5a24c8dfdc8413
Ry Ferguson <rytf12@gmail.com>
2012-11-10 22:47:33 -0800

    remove action attribute from forms

--- deploy/this-site/html/newSection.php
+++ deploy/this-site/html/newSection.php
@@ -4,9 +4,9 @@ require_once(THIS_SITE . 'html/error.php');
 require_once(THIS_SITE . 'html/sectionInputs.php');
 
 function newSection($formData, $error = '') {
-    return sprintf('%s<form action="%s" method="post">%s'
+    return sprintf('%s<form method="post">%s'
         . '<input type="submit" value="Create New Section"/></form>',
-        error($error), NEW_SECTION, sectionInputs($formData));
+        error($error), sectionInputs($formData));
 }
 
 ?>
