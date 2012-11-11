dc5b30248dd1b1a09ed10a6201dfa8cd834056f5
Ry Ferguson <rytf12@gmail.com>
2012-11-11 14:30:31 -0800

    use autofocus()

--- deploy/cms/new-section.php
+++ deploy/cms/new-section.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once(STD_LIB . 'html/autofocus.php');
 require_once(THIS_SITE . 'forms/SectionValidator.php');
 require_once(THIS_SITE . 'html/newSection.php');
 require_once(THIS_SITE . 'html/newSectionCreated.php');
@@ -21,7 +22,8 @@ else {
     $formData = $validator->values();
     $formData['link_order'] = $sectionModel->getMaxLinkOrder() + 1;
     $content = newSection($formData);
-    $autofocus = '<script>document.getElementById("link_title").focus();</script>';
+    $autofocus = autofocus('link_title');
+    //$autofocus = '<script>document.getElementById("link_title").focus();</script>';
 }
 
 ?>
