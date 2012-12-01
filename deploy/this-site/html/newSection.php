09227c2d24d3fbcba1b230b2e78923488e2a0fc3
Ry Ferguson <rytf12@gmail.com>
2012-12-01 02:15:12 -0800

    refactor new section html

--- deploy/this-site/html/newSection.php
+++ deploy/this-site/html/newSection.php
@@ -4,9 +4,15 @@ require_once(CITYPHP . 'html/error.php');
 require_once(THIS_SITE . 'html/sectionInputs.php');
 
 function newSection($formData, $error = '') {
-    return sprintf('<form method="post">%s%s'
-        . '<input type="submit" value="Create New Section"/></form>',
-        error($error), sectionInputs($formData));
+    ob_start(); ?>
+
+<form method="post">
+    <?=error($error)?>
+    <?=sectionInputs($formData)?>
+    <input type="submit" value="Create New Section"/>
+</form>
+
+    <? return ob_get_clean();
 }
 
 ?>
