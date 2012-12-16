6f833569de46abe16b20bc352f4472673f55e8c9
Ry Ferguson <rytf12@gmail.com>
2012-12-16 01:45:06 -0800

    add space after VALUES in SQL

--- deploy/this-site/database/SectionModel.php
+++ deploy/this-site/database/SectionModel.php
@@ -27,7 +27,7 @@ class SectionModel extends DatabaseAdapter {
 
         $this->query(sprintf('INSERT INTO %s (url_id, link_title, html_title,
             html_description, html_keywords, page_content, link_order, display_mode)
-            VALUES("%s", "%s", "%s", "%s", "%s", "%s", %d, %d)',
+            VALUES ("%s", "%s", "%s", "%s", "%s", "%s", %d, %d)',
             TABLE_SECTIONS,
             $this->esc($data['url_id']),
             $this->esc($data['link_title']),
