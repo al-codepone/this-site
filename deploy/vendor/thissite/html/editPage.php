c290f0ec12200effd8be4bb5d847029c941a4649
Ry Ferguson <rytf12@gmail.com>
2013-09-15 20:50:26 -0700

    move view page link to top

--- deploy/vendor/thissite/html/editPage.php
+++ deploy/vendor/thissite/html/editPage.php
@@ -9,12 +9,12 @@ function editPage($formData, $currentPage, $errors = array()) {
 <form method="post" id="page_form">
     <input type="hidden" name="delete_flag" value="0"/>
     <?=ulist($errors, array('class' => 'error'))?>
+    <div><a href="<?=ROOT . $currentPage['url_id']?>">View Page</a></div>
     <?=pageInputs($formData)?>
     <div>
         <input type="submit" value="Save"
         /><input type="button" value="Delete" onClick="deletePage();"/>
     </div>
-    <div><a href="<?=ROOT.$currentPage['url_id']?>">View Page</a></div>
 </form>
 
     <?php return ob_get_clean();
