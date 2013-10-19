60ecd425452b48ab0e0b22b239d3ba5f02eeacca
Ry Ferguson <rytf12@gmail.com>
2013-10-18 22:48:10 -0700

    remove css type

--- deploy/vendor/thissite/html/template.php
+++ deploy/vendor/thissite/html/template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width"/>
-        <link type="text/css" rel="stylesheet" href="<?=CSS?>styles.css"/>
+        <link rel="stylesheet" href="<?=CSS?>styles.css"/>
         <?=$head?>
     </head>
     <body>
