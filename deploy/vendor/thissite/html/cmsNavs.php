7002c7f03fd3b27c83bd02549eb1652b6689ab11
Ry Ferguson <rytf12@gmail.com>
2013-10-24 03:46:12 -0700

    add bottom margin to new page link

--- deploy/vendor/thissite/html/cmsNavs.php
+++ deploy/vendor/thissite/html/cmsNavs.php
@@ -9,11 +9,14 @@ function cmsNavs($pages, $currentPageID, $isNewPage) {
     list($listItems, $selectOptions, $selectedValue)
         = navElements($pages, $currentPageID, EDIT_PAGE, 'page_id', true);
 
-    array_unshift($listItems,
-        sprintf('<a %shref="%s">%s</a>',
-            currentLink($isNewPage),
-            NEW_PAGE,
-            NEW_PAGE_TITLE));
+    $newPageLink = sprintf('<a %shref="%s">%s</a>',
+        currentLink($isNewPage),
+        NEW_PAGE,
+        NEW_PAGE_TITLE);
+
+    $listItems = array_merge(
+        array($newPageLink => array('id' => 'new_page')),
+        $listItems);
 
     $selectOptions = array_merge(
         array(NEW_PAGE => NEW_PAGE_TITLE),
