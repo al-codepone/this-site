1cee36e78c3af6204a70b60e682d00034d21bf0d
Ry Ferguson <rytf12@gmail.com>
2013-12-18 22:59:44 -0800

    fix spelling

--- deploy/vendor/thissite/html/newPageCreated.php
+++ deploy/vendor/thissite/html/newPageCreated.php
@@ -5,7 +5,7 @@ require_once CITYPHP . 'html/blist.php';
 function newPageCreated($pageID, $urlID) {
     return '<div class="success">New page created</div>'
         . blist(array(
-            sprintf('<a href="%s%s">View Pagess</a>', ROOT, $urlID),
+            sprintf('<a href="%s%s">View Page</a>', ROOT, $urlID),
             sprintf('<a href="%s%d">Edit Page</a>', EDIT_PAGE, $pageID),
             sprintf('<a href="%s">%s</a>', NEW_PAGE, NEW_PAGE_TITLE)));
 }
