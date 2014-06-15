2f455d7dde844ab194b67e33fc75e9ace19ba0fb
Ry Ferguson <rytf12@gmail.com>
2014-06-14 22:20:31 -0700

    refactor page updated

--- src/thissite/html/pageUpdated.php
+++ src/thissite/html/pageUpdated.php
@@ -1,8 +1,9 @@
 <?php
 
 function pageUpdated($pageID, $urlID) {
-    return '<div class="success">Page updated</div>'
-        . blist(array(
-            sprintf('<a href="%s%s">View Page</a>', ROOT, $urlID),
-            sprintf('<a href="%s%d">Edit Page</a>', EDIT_PAGE, $pageID)));
+    return
+        '<div class="success">Page updated</div>' .
+        c\ulist(
+            c\hlink(ROOT . $urlID, 'View Page'),
+            c\hlink(EDIT_PAGE . $pageID, 'Edit Page'));
 }
