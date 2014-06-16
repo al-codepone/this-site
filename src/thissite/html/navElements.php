58f73d55b63264c0af41c519c8b7f3d0b3cf4a06
Ry Ferguson <rytf12@gmail.com>
2014-06-15 21:56:38 -0700

    make hidden link for current page show in drop down nav

--- src/thissite/html/navElements.php
+++ src/thissite/html/navElements.php
@@ -6,9 +6,10 @@ function navElements($pages, $currentPageID, $baseURL, $key, $forceShow = false)
     $selectedValue = null;
 
     foreach($pages as $page) {
+        $isCurrent = ($page['page_id'] == $currentPageID);
+        $url = $baseURL . $page[$key];
+
         if($forceShow || $page['display_mode'] == 1) {
-            $isCurrent = ($page['page_id'] == $currentPageID);
-            $url = $baseURL . $page[$key];
             $listItems[] = c\a(
                 array_merge(
                     currentLink($isCurrent),
@@ -21,6 +22,16 @@ function navElements($pages, $currentPageID, $baseURL, $key, $forceShow = false)
                 $selectedValue = $url;
             }
         }
+
+        if($isCurrent) {
+            $currentTitle = $page['link_title'];
+            $currentURL = $url;
+        }
+    }
+
+    if(is_null($selectedValue) && !is_null($currentTitle)) {
+        $selectOptions[$currentURL] = $currentTitle;
+        $selectedValue = $currentURL;
     }
 
     return array($listItems, $selectOptions, $selectedValue);
