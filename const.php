29d73bbb4d97fd81e9e30d4c01e68f37cea2d6bc
Ry Ferguson <rytf12@gmail.com>
2014-07-29 20:36:13 -0700

    remove unused MYSQL_DEBUG constant

--- const.php
+++ const.php
@@ -12,7 +12,6 @@ define('MYSQL_HOST', 'localhost');
 define('MYSQL_USERNAME', 'juanita');
 define('MYSQL_PASSWORD', 'pasta');
 define('MYSQL_DBNAME', 'ts_test');
-define('MYSQL_DEBUG', false);
 
 define('IS_SAFE_MODE', false);
 define('NEW_PAGE_TITLE', 'New Page');
